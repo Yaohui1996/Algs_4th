a37c0ae7dcd1b7d4c1856674ad175fcf24f03ee2
Yaohui1996 <zggdscutlyh@163.com>
2022-10-06 17:22:35 +0800

    update

--- Exercise/src/Ex_01_01_01/Main.java
+++
@@ -1,7 +0,0 @@
-package Ex_01_01_01;
-
-public class Main {
-    public static void main(String[] agrs) {
-        System.out.println((0 + 15) / 2);
-    }
-}
---
+++ Exercise/src/Ex_01_01_02.java
@@ -0,0 +1,5 @@
+public class Ex_01_01_02 {
+    public static void main(String[] args) {
+        System.out.println("Ex_01_01_02");
+    }
+}
